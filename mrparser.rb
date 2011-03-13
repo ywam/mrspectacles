1a991054b410d826d68354c379c783054aa60cbb
Chris <chris@codecafe.com>
2011-03-13 22:22:54 +1300

    configuration file path change

--- mrparser.rb
+++ mrparser.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'active_record'
 require 'yaml'
 
-dbconfig = YAML::load(File.open('database.yml'))
+dbconfig = YAML::load(File.open('db/database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
 
 class Spectacle < ActiveRecord::Base
